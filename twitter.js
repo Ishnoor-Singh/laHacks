1ae66f8e2d965073d29c4654b164c2ca42639286
Vaibhav-4646 <vaibhav.trico@gmail.com>
2019-03-30 09:00:54 -0700

    commit

--- twitter.js
+++ twitter.js
@@ -4,6 +4,38 @@ const TWITTER_SECRET_KEY = "YxfpyTh19xyOcyBPkDsdP9SmnviQXRBMkkW3b28jpzWeN6ooON";
 const TWITTER_TOKEN = "1057692323239186432-GLPJ2V0hPjzAvYKfn8n6rsynqFAypL";
 const TWITTER_SECRET_TOKEN = "UzD9kjkbhmLsWD2SyA5G6dCtpp2dzQFtGOcE4wZW4m1EN";
 
+function getText (tweet)
+{
+  if (tweet.truncated == false)
+  {
+    console.log(tweet.text)
+  }
+  else
+  {
+    foo(tweet);
+  }
+}
+async function foo (tw)
+{
+  var txt = "error";
+  txt = await bar(tw);
+  console.log(txt);
+}
+
+
+function bar (t){
+  var param = {id: t.id_str, tweet_mode: 'extended'};
+  client.get('statuses/show.json', param, function(error, tweets, response) {
+    if (!error) {
+      console.log(t)
+      return(t.text);
+    }
+    else
+    {
+      console.log(error)
+    }
+  });
+}
 var Twitter = require('twitter');
 var test_id = "realDonaldTrump";
 var client = new Twitter({
@@ -16,6 +48,10 @@ var client = new Twitter({
   var params = {screen_name: test_id};
   client.get('statuses/user_timeline.json?count=2', params, function(error, tweets, response) {
     if (!error) {
-      console.log(tweets);
+      tweets.map(x=>getText(x));
+    }
+    else
+    {
+      console.log(error);
     }
   });
